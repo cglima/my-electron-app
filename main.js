b0c7bf37c47b98b38c18512de6776c024d2b8360
Cassiana <lima.cglima@gmail.com>
2023-05-07 14:45:33 -0300

    iniciando a janela de exibição

---
+++ main.js
@@ -0,0 +1,16 @@
+// import
+const { app, BrowserWindow } = require("electron");
+
+// função creatWindow
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
